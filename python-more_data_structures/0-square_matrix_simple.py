db65966780ddc894c724d0699f00834e7be5e85d
Henriette Iraguha <h.iraguha@alustudent.com>
2025-10-27 12:39:48 -0700

    updating some changes on file 0-main.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
-        results = [i ** i for i in row]
+        results = [i ** 2 for i in row]
         new_matrix.append(results)
     return new_matrix
